90521113e4d54f9d7ae5634197c4b32c9b8d7856
qharo <aditya.nair24@gmail.com>
2023-01-15 23:36:22 +0100

    smol change in exp 5

--- exp5.py
+++ exp5.py
@@ -5,7 +5,7 @@ from models import *
 def EXP5(X, Y, verbose):    
     if verbose:
         myLog.heading("EXP 5: RFC")
-        myLog.indent(1, "We choose 4 values of estimators: 1, 5, 10, 100")
+        myLog.indent(1, "We choose 4 values of estimators: 1, 5, 25, 100")
     kfolds = kf(n_splits=4, shuffle=True)
 
     train_indices = []
@@ -17,6 +17,15 @@ def EXP5(X, Y, verbose):
 
     
     xTrain, xTest, yTrain, yTest = indexToSplit(X, Y, train_indices[0], test_indices[0])
+
+    TEST_VALUES = [1, 5, 25, 100]
+
+    for i in range(0, 4):
+        xTrain, xTest, yTrain, yTest = indexToSplit(X, Y, train_indices[i], test_indices[i])
+        if verbose: 
+            myLog.indent(2, f"{TEST_VALUES[i]} TREES")
+        RFCModel(xTrain, xTest, yTrain, yTest, TEST_VALUES[i], verbose)
+
 
     TEST_VALUES = [100, 500, 250, 750]
 
